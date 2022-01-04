1dc3f405903c3469f5d36112fc2d3d8e7284fea9
ilmari99 <i.vahteristo@gmail.com>
2022-01-04 20:24:44 +0200

    Added random_state argument for test-train split.

--- model_optimizer_tools.py
+++ model_optimizer_tools.py
@@ -24,9 +24,9 @@ def test_learning_speed(model, x_train, y_train,samples=500, validation_split=0.
     if not model.built:
         OptimizedModel.OptimizedModel.build_and_compile(model,np.shape(x_train))
     samples = len(y_train)#TODO change to numpy function
-    epochs = 15
+    epochs = constants.TEST_EPOCHS
     verbose = 0
-    x_train, x_test, y_train, y_test = train_test_split(x_train[0:samples], y_train[0:samples],test_size=0.2)
+    x_train, x_test, y_train, y_test = train_test_split(x_train[0:samples], y_train[0:samples],test_size=0.1, random_state=42)
     cb_loss = ccb.loss_callback()
     start = time.time()
     hist = model.fit(
@@ -39,7 +39,7 @@ def test_learning_speed(model, x_train, y_train,samples=500, validation_split=0.
         shuffle=True
     )
     elapsed_time = time.time() - start
-    print(f"Elapsed time: {elapsed_time}")
+    #print(f"Elapsed time: {elapsed_time}")
     #cb_loss.plot_loss()
     
     #if only one epoch is done, returns the last loss
