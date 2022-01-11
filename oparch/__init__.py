fbc8062957fb57e075ec02ad2707bb358ba48566
ilmari99 <i.vahteristo@gmail.com>
2022-01-11 20:54:06 +0200

    Tried to suppress CUDA devices information.

--- oparch/__init__.py
+++ oparch/__init__.py
@@ -1,7 +1,8 @@
-from pathlib import Path
-import sys
 from . import model_optimizer
 from . import OptimizedModel
 from . import model_optimizer_tools
 from . import LossCallback
 from . import configurations
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = "3"
