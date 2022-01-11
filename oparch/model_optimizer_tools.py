cdd73b6ed0c6e2eee2c46d6288e65c219ccd6060
ilmari99 <i.vahteristo@gmail.com>
2022-01-12 00:47:01 +0200

    Saves models weights before training it, and loads the weights after. Hence, models weights do not change in training. Now there are many redundancies, that have been created to solve this issue.

--- oparch/model_optimizer_tools.py
+++ oparch/model_optimizer_tools.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 
 def test_learning_speed(model, x_train, y_train,samples=500, validation_split=0.2):
     """Tests the learning speed of the model.
-       The learning speed is measured by the loss on the validation set
+       Returns configurations.LOSS_METRIC from LossCallback
 
     Args:
         model (Sequential): Doesn't have to be built
@@ -20,23 +20,32 @@ def test_learning_speed(model, x_train, y_train,samples=500, validation_split=0.
         float: if epochs == 1 returns the loss on validation set.
         else returns configurations.LOSS_METRIC.
     """
-    if not model.built:
-        OptimizedModel.build_and_compile(model,np.shape(x_train))
+    try:
+        model.save_weights("test_weights.h5")
+        #print("*******Model is already build*******")
+    except ValueError:
+        pass
+    OptimizedModel.build_and_compile(model,np.shape(x_train))
+    model.save_weights("test_weights.h5")
     samples = len(y_train)#TODO change to numpy function
     verbose = 0
-    x_train, x_test, y_train, y_test = train_test_split(x_train[0:samples], y_train[0:samples],test_size=0.1, random_state=42)
+    validation_data = None
+    if("VALIDATION" in configurations.LEARNING_METRIC):
+        x_train, x_test, y_train, y_test = train_test_split(x_train[0:samples], y_train[0:samples],test_size=0.2, random_state=42)
+        validation_data = (x_test,y_test)
     cb_loss = lcb.LossCallback(samples=y_train.shape[0])
     start = time.time()
     hist = model.fit(
         x_train, y_train,
         epochs=configurations.TEST_EPOCHS,
         verbose=verbose,
-        validation_data=(x_test,y_test),
+        validation_data=validation_data,
         batch_size=configurations.BATCH_SIZE,
         callbacks=[cb_loss],
         shuffle=True,
         use_multiprocessing=True,
     )
+    model.load_weights("test_weights.h5")
     elapsed_time = time.time() - start
     #print(f"Elapsed time: {elapsed_time}")
     #cb_loss.plot_loss()
