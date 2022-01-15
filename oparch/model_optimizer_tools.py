5bda15101d47f6a04e6bbcb5ad894e5d1a734040
ilmari99 <i.vahteristo@gmail.com>
2022-01-15 18:44:07 +0200

    Added opt_dense, opt_activation, and check_compilation

--- oparch/model_optimizer_tools.py
+++ oparch/model_optimizer_tools.py
@@ -22,14 +22,17 @@ def test_learning_speed(model: tf.keras.Model, x_train: np.ndarray,
         else returns configurations.LOSS_METRIC.
     """
     try:
-        model.save_weights("test_weights.h5")
-        #print("*******Model is already build*******")
-    except ValueError:
-        pass
-    OptimizedModel.build_and_compile(model,np.shape(x_train))
+        model.optimizer.get_weights()
+    except AttributeError:
+        raise AttributeError("The model must be built before testing the learning speed")
+    #rebuild and compile the model to get a clean optimizer
+    if model.optimizer.get_weights(): #If list is not empty or model has not been
+        model.build(np.shape(x_train))
+        model.compile(optimizer=type(model.optimizer)(model.optimizer.get_config()["learning_rate"]),
+                  loss=model.loss)
+    #Save the models weights to return the model to its original state after testing the learning speed
     model.save_weights("test_weights.h5")
-    optimizer_state = model.optimizer.get_weights()
-    samples = np.shape(y_train)[0] #TODO change to numpy function
+    samples = np.shape(y_train)[0] #TODO: this uses all available data instead of samples
     verbose = 0
     validation_data = None
     if("VALIDATION" in configurations.LEARNING_METRIC): #If the learning metric should be calculated from validation set
@@ -47,12 +50,13 @@ def test_learning_speed(model: tf.keras.Model, x_train: np.ndarray,
         shuffle=True,
         use_multiprocessing=True,
     )
-    after_training_optimizer_state = model.optimizer.get_weights()
-    model.load_weights("test_weights.h5")
     elapsed_time = time.time() - start
-    
-    #The model has random weigths here, but its optimizer still has a state from the training
-    OptimizedModel.build_and_compile(model, np.shape(x_train))
+    #Load the weights the model had when it came to testing, so testing doesn't affect the model itself
+    #Rebuild and recompile to give the model a clean optimizer
+    model.load_weights("test_weights.h5")
+    model.build(np.shape(x_train))
+    model.compile(optimizer=type(model.optimizer)(model.optimizer.get_config()["learning_rate"]),
+                  loss=model.loss)
     #if only one epoch is done, returns the last loss
     if(configurations.TEST_EPOCHS==1):
         return cb_loss.learning_metric["LAST_LOSS"]
