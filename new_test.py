7e3c495c670585f4ef7bc818b51e78053d2265da
ilmari99 <i.vahteristo@gmail.com>
2022-01-15 21:21:31 +0200

    Updated

--- new_test.py
+++ new_test.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-from oparch import model_optimizer as mod_op
+from oparch import optimize as opt
 def y_function(x) -> np.ndarray:
     y = np.zeros(max(np.shape(x))) #Assumes more training samples than features which should be true
     features = len(x[0])
@@ -14,11 +14,13 @@ def y_function(x) -> np.ndarray:
 np.random.seed(seed=42) #for reproducibility
 X = np.random.rand(300,4)
 y = y_function(X)
-    
+
 layers = [tf.keras.layers.Dense(1),tf.keras.layers.Dropout(0.1),tf.keras.layers.Dense(1)]
 model = tf.keras.models.Sequential(layers)
 model.build(np.shape(X))
 model.compile(loss=tf.keras.losses.MeanSquaredError(),optimizer=tf.keras.optimizers.SGD(0.1))
-(configuration, metric) = mod_op.opt_dense(model, 0, X, y)
-print(configuration)
-print(metric)
+(configuration, metric) = opt.opt_learning_rate(model, X, y)
+print(configuration, metric)
+#(configuration, metric) = mod_op.opt_dense(model, 0, X, y)
+#print(configuration)
+#print(metric)
