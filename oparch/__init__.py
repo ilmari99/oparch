22b2de3f838060b51ddbc77bce80fe59a3ae9378
ilmari99 <i.vahteristo@gmail.com>
2022-01-17 23:45:17 +0200

    Added numpy and tensorflow seeds for reproducibility

--- oparch/__init__.py
+++ oparch/__init__.py
@@ -4,5 +4,9 @@ from . import model_optimizer_tools
 from . import LossCallback
 from . import configurations
 import os
+import numpy as np
+import tensorflow as tf
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+np.random.seed(seed=42) #for reproducibility
+tf.random.set_seed(42)
