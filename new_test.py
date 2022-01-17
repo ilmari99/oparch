88e409bf6cbba76d60bf9410a5317dd439937718
ilmari99 <i.vahteristo@gmail.com>
2022-01-17 23:51:27 +0200

    Sandbox

--- new_test.py
+++ new_test.py
@@ -5,21 +5,19 @@ from oparch import optimize as opt
 from sklearn.model_selection import train_test_split
 from oparch import configurations
 from oparch import LossCallback
+import pandas as pd
 def y_function(x) -> np.ndarray:
     y = np.zeros(max(np.shape(x))) #Assumes more training samples than features which should be true
     features = len(x[0])
-    coeffs = np.array([(c+1)*0.5 for c in range(features)])
+    coeffs = np.random.uniform(low=-1, high=1, size=(1,features))[0]#np.array([(c+1)*0.5 for c in range(features)])
     for i,row in enumerate(x):
-        coeffs_randomness = np.random.uniform(low=0.1, high=2, size=(1,features))
+        coeffs_randomness = np.random.uniform(low=0.8, high=1.2, size=(1,features))
         y[i] = np.sum(coeffs*row*coeffs_randomness[0], axis=0) #Add some randomness
-        #y = 1/(1+np.exp(-y))
+    y = 1/(1+np.exp(-y)) #Sigmoid
     return y
-
-np.random.seed(seed=42) #for reproducibility
 X = np.random.rand(600,10)
 y = y_function(X)
-
-layers = [tf.keras.layers.Dense(1),tf.keras.layers.Dropout(0.1),tf.keras.layers.Dense(1)]
+layers = [tf.keras.layers.Dense(1),tf.keras.layers.Dropout(0.1),tf.keras.layers.Dense(1,activation="sigmoid")]
 model = tf.keras.models.Sequential(layers)
 model.build(np.shape(X))
 model.compile(loss=tf.keras.losses.MeanSquaredError(),optimizer=tf.keras.optimizers.RMSprop())
@@ -35,18 +33,23 @@ hist = model.fit(
         use_multiprocessing=True,
     )
 y_pred = model.predict(X_test)
+
+
 print(y_pred[:9])
 print(y_test[:9])
 plt.plot(range(len(y_pred)),y_pred)
 plt.plot(range(len(y_test)),y_test)
 plt.figure(2)
-model.compile(loss=tf.keras.losses.MeanSquaredError(),optimizer=tf.keras.optimizers.RMSprop())
+(loss_fun, loss) = opt.opt_loss_fun(model, X, y)
+model.compile(loss=loss_fun,optimizer=tf.keras.optimizers.RMSprop())
 (decay,loss) = opt.opt_decay(model, X, y)
 print(decay,loss)
 model.compile(loss=tf.keras.losses.MeanSquaredError(),optimizer=tf.keras.optimizers.RMSprop(decay=decay))
 (lr, loss_lr) = opt.opt_learning_rate(model, X, y)
 model.compile(loss=tf.keras.losses.MeanSquaredError(),optimizer=tf.keras.optimizers.RMSprop(lr,decay=decay))
 print(lr,loss_lr)
+
+
 index = 0
 (config, act_loss) = opt.opt_activation(model, index, X, y)
 new_layer = tf.keras.layers.Dense.from_config(config)
@@ -55,17 +58,8 @@ layers = opt.get_copy_of_layers(layers)
 model = tf.keras.models.Sequential(layers)
 model.build(np.shape(X))
 model.compile(loss=tf.keras.losses.MeanSquaredError(),optimizer=tf.keras.optimizers.RMSprop(lr,decay=decay))
-(config, loss_units) = opt.opt_dense_units(model, 0, X, y)
-if config != None:
-    new_layer = tf.keras.layers.Dense.from_config(config)
-    layers[0] = new_layer
-else:
-    layers.pop(0)
-layers = opt.get_copy_of_layers(layers)
-model = tf.keras.models.Sequential(layers)
-model.build(np.shape(X))
-model.compile(loss=tf.keras.losses.MeanSquaredError(),optimizer=tf.keras.optimizers.RMSprop(lr,decay=decay))
-
+(model, loss_units) = opt.opt_dense_units(model, 0, X, y,return_model=True)
+'''
 index = 2
 if config==None:
     index = 1
@@ -79,11 +73,11 @@ layers = opt.get_copy_of_layers(layers)
 model = tf.keras.models.Sequential(layers)
 model.build(np.shape(X))
 model.compile(loss=tf.keras.losses.MeanSquaredError(),optimizer=tf.keras.optimizers.RMSprop(lr,decay=decay))
-
+'''
 model.summary()
 hist = model.fit(
         X, y,
-        epochs=15,
+        epochs=60,
         verbose=1,
         validation_data=(X_test,y_test),
         batch_size=configurations.BATCH_SIZE,
@@ -91,6 +85,7 @@ hist = model.fit(
         shuffle=True,
         use_multiprocessing=True,
     )
+opt.print_optimized_model(model)
 y_pred = model.predict(X_test)
 print(y_pred)
 plt.plot(range(len(y_pred)),y_pred)
