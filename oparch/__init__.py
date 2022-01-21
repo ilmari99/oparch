e1b57e3d35d642ada392f89e963a8ddaae8ba2c9
ilmari99 <i.vahteristo@gmail.com>
2022-01-21 15:55:25 +0200

    Added Python built-in random seed 42, now imports functions from .optimize so the user can just import oparch and refer to the functions at optimize without using the dot notation as oparch.optimize. ...

--- oparch/__init__.py
+++ oparch/__init__.py
@@ -1,12 +1,14 @@
 from . import optimize
-from . import OptimizedModel
-from . import model_optimizer_tools
+from . import optimize_utils
 from . import LossCallback
 from . import configurations
+from .optimize import *
 import os
 import numpy as np
 import tensorflow as tf
+import random
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-np.random.seed(seed=42) #for reproducibility
+np.random.seed(seed=42) #Also sets sk.learn random number
 tf.random.set_seed(42)
+random.seed(42)
