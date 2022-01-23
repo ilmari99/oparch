77b250a88780d3fcc7050bd93336e4d65a42d302
ilmari99 <i.vahteristo@gmail.com>
2022-01-24 00:47:36 +0200

    Made the tests folder a dictionary, so tests could be run in batches as well as all at once.

---
+++ oparch_tests/__init__.py
@@ -0,0 +1,22 @@
+'''
+from pathlib import Path
+import sys
+path_root = Path(__file__).parents[2]
+sys.path.append(str(path_root))
+print(sys.path)
+#"C:\\Users\\ivaht\\Desktop\\PYTHON\\Python_scripts\\optarch\\oparch_tests"
+'''
+from . import test_utils
+import unittest
+import oparch
+import os
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot
+import sklearn
+import random
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+np.random.seed(seed=42) #Also sets sk.learn random number
+tf.random.set_seed(42)
+random.seed(42)
