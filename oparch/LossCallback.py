5afea2315f87a57bf47bd608b6b0f699b95d7e4b
ilmari99 <i.vahteristo@gmail.com>
2022-01-24 19:42:10 +0200

    Fixed issues with using the callback with no sample size specified. Also fixed issues with using the callback with a VALIDATION learning metric.

--- oparch/LossCallback.py
+++ oparch/LossCallback.py
@@ -18,19 +18,18 @@ class LossCallback(tf.keras.callbacks.Callback):
     
     def __init__(self, **kwargs):
         super().__init__()
-        epochs = kwargs.get("epochs",configurations.TEST_EPOCHS)
-        verbose = kwargs.get("verbose",0)
-        samples = kwargs.get("samples",0)
-        self.verbose = verbose
-        self.samples = samples
+        self.epochs = kwargs.get("epochs",configurations.TEST_EPOCHS)
+        self.verbose = kwargs.get("verbose",0)
+        self.samples = kwargs.get("samples",0)
+        self.batch_size = kwargs.get("batch_size",configurations.BATCH_SIZE)
         self.current_epoch = 0
         self.epoch_start = 0
-        if samples>0:
-            self.batch_count = int(np.ceil((samples/configurations.BATCH_SIZE)))
-            self.loss_array_epoch = np.zeros(epochs)
-            self.loss_array_validation = np.zeros(epochs)
-            self.accuracy_array_validation = np.zeros(epochs)
-            self.loss_array_batch = np.zeros(self.batch_count * configurations.TEST_EPOCHS)
+        if self.samples>0:
+            self.batch_count = int(np.ceil((self.samples/self.batch_size)))
+            self.loss_array_epoch = np.zeros(self.epochs)
+            self.loss_array_validation = np.zeros(self.epochs)
+            self.accuracy_array_validation = np.zeros(self.epochs)
+            self.loss_array_batch = np.zeros(self.batch_count * self.epochs)
         else:
             print("It is recommended to specify sample size when creating the LossCallback")
             self.loss_array_epoch = []
@@ -42,9 +41,11 @@ class LossCallback(tf.keras.callbacks.Callback):
         self.learning_metric["LAST_LOSS"] = self.loss_array_epoch[-1]
         self.learning_metric["AVERAGE_LOSS_BATCH"] = np.mean(self.loss_array_batch)
         #Compare the difference in loss to the previous loss
-        self.learning_metric["RELATIVE_IMPROVEMENT_EPOCH"] = np.mean(np.diff(self.loss_array_epoch)/self.loss_array_epoch[0:-1])
-        self.learning_metric["RELATIVE_IMPROVEMENT_BATCH"] = np.mean(np.diff(self.loss_array_batch)/self.loss_array_batch[0:-1])
-        if "VALIDATION" in configurations.LEARNING_METRIC:
+        if self.epochs > 1 and self.batch_count > 1:
+            self.learning_metric["RELATIVE_IMPROVEMENT_EPOCH"] = np.mean(np.diff(self.loss_array_epoch)/self.loss_array_epoch[0:-1])
+            self.learning_metric["RELATIVE_IMPROVEMENT_BATCH"] = np.mean(np.diff(self.loss_array_batch)/self.loss_array_batch[0:-1])
+        self.loss_array_validation = [i for i in self.loss_array_validation if i is not None] #If validations are empty
+        if self.loss_array_validation:
             self.learning_metric["VALIDATION_LOSS"] = np.mean(self.loss_array_validation)
             self.learning_metric["LAST_VALIDATION_LOSS"] = self.loss_array_validation[-1]
             self.learning_metric["VALIDATION_ACCURACY"] = np.mean(self.accuracy_array_validation)
@@ -55,7 +56,7 @@ class LossCallback(tf.keras.callbacks.Callback):
 
     def on_train_batch_end(self, batch, logs=None):
         if self.samples == 0:
-            self.loss_array_batch.append(logs["loss"])
+            self.loss_array_batch.append(logs.get("loss"))
         else:
             self.loss_array_batch[batch + self.current_epoch*self.batch_count] = logs["loss"]
             
@@ -65,15 +66,13 @@ class LossCallback(tf.keras.callbacks.Callback):
 
     def on_epoch_end(self, epoch, logs=None):
         if self.samples == 0:
-            self.loss_array_epoch.append(logs["loss"])
-            if "VALIDATION" in configurations.LEARNING_METRIC:
-                self.loss_array_validation.append(logs["val_loss"])
-                self.accuracy_array_validation.append(logs["val_accuracy"])
+            self.loss_array_epoch.append(logs.get("loss"))
+            self.loss_array_validation.append(logs.get("val_loss"))
+            self.accuracy_array_validation.append(logs.get("val_accuracy"))
         else:
-            self.loss_array_epoch[epoch] = logs["loss"]
-            if "VALIDATION" in configurations.LEARNING_METRIC:
-                self.loss_array_validation[epoch] = logs["val_loss"]
-                self.accuracy_array_validation[epoch] = logs["val_accuracy"]
+            self.loss_array_epoch[epoch] = logs.get("loss")
+            self.loss_array_validation[epoch] = logs.get("val_loss")
+            self.accuracy_array_validation[epoch] = logs.get("val_accuracy")
         if self.verbose == 2:
             elaps_time = time.time() - self.epoch_start
             print(f"Elapsed time in epoch: {elaps_time}")
@@ -83,4 +82,4 @@ class LossCallback(tf.keras.callbacks.Callback):
         plt.plot(range(len(self.loss_array_batch)), self.loss_array_batch)
         plt.xlabel("Batches")
         plt.ylabel("Loss on batch")
-        #plt.show()
+        
