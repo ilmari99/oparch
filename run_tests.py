9a089bfb591e3e28aa44109bc69b1f9766ffdb9e
ilmari99 <i.vahteristo@gmail.com>
2022-01-26 16:18:44 +0200

    Added import from test_optimize_funs

--- run_tests.py
+++ run_tests.py
@@ -5,5 +5,6 @@ from oparch import optimize_utils as utils
 import tensorflow.keras as tf
 
 from oparch_tests.test_utils import Test_optimize_utils
+from oparch_tests.test_optimize import Test_optimize_funs
 
 unittest.main()
