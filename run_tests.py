c2ab716df723a1f43d883cb9afe23c42844bdb9e
ilmari99 <i.vahteristo@gmail.com>
2022-01-26 17:53:29 +0200

    Running creates a file 'test_log.log'

--- run_tests.py
+++ run_tests.py
@@ -1,10 +1,12 @@
 import oparch
 import unittest
 import oparch_tests
+import sys
 from oparch import optimize_utils as utils
 import tensorflow.keras as tf
-
 from oparch_tests.test_utils import Test_optimize_utils
-from oparch_tests.test_optimize import Test_optimize_funs
-
-unittest.main()
+from oparch_tests.test_opt_learning_rate import Test_opt_learning_rate
+from oparch_tests.test_opt_loss_fun import Test_opt_loss_fun
+with open("test_log.log","w") as sys.stdout:
+    unittest.main()
+sys.stdout = sys.__stdout__
