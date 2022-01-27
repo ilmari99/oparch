98f2d8ea4220ccad847031574e22c43edc29a62c
ilmari99 <i.vahteristo@gmail.com>
2022-01-27 14:22:01 +0200

    updated

--- run_tests.py
+++ run_tests.py
@@ -7,6 +7,7 @@ import tensorflow.keras as tf
 from oparch_tests.test_utils import Test_optimize_utils
 from oparch_tests.test_opt_learning_rate import Test_opt_learning_rate
 from oparch_tests.test_opt_loss_fun import Test_opt_loss_fun
+from oparch_tests.test_opt_activation import Test_opt_activation
 with open("test_log.log","w") as sys.stdout:
     unittest.main()
 sys.stdout = sys.__stdout__
