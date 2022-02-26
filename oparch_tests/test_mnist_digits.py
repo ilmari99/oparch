c0a874dcf82e84639e6429f0712b10209247d872
ilmari99 <i.vahteristo@gmail.com>
2022-02-26 16:16:58 +0200

    updated

--- oparch_tests/test_mnist_digits.py
+++ oparch_tests/test_mnist_digits.py
@@ -17,55 +17,85 @@ from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
 (X,y),(X_test,y_test) = mnist.load_data()
 X = X.reshape(-1,28,28,1)
 X_test = X_test.reshape(-1,28,28,1)
-X = X.astype('float32') / 255
-X_test = X_test.astype('float32') / 255
+#X = X.astype('float32') / 255
+#X_test = X_test.astype('float32') / 255
 X = X/255
 X_test = X_test/255
+'''
 layers = [
-    Conv2D(32,(3,3),activation=tf.keras.activations.relu),
+    Conv2D(64,(5,5),strides=(1,1)),
+    MaxPooling2D((2,1),strides=(2,1)),
+    Conv2D(64, (3,3),strides=(1,1)),
+    MaxPooling2D((2,1),strides=(2,2)),
+    Conv2D(64,(3,3),strides=(1,1)),
+    Flatten(),
+    Dense(1),
+    Dense(1),
+    Dense(64,activation=tf.keras.activations.relu),
+    Dense(64),
+    Dense(10,activation=tf.keras.activations.softmax),
+]'''
+layers = [
+    Conv2D(32,(3,3)),
     MaxPooling2D((2,2)),
     Conv2D(32, (3,3)),
     MaxPooling2D((2,2)),
-    Conv2D(16,(3,3)),
+    Conv2D(32,(3,3)),
     Flatten(),
-    Dense(64,activation=tf.keras.activations.relu),
-    Dense(16),
+    Dense(1),
+    Dense(1),
+    Dense(1,activation=tf.keras.activations.relu),
+    Dense(1),
     Dense(10,activation=tf.keras.activations.softmax),
 ]
+
 layers = opt.utils.get_copy_of_layers(layers)
 
 model = tf.keras.models.Sequential(layers)
 model.build(X.shape)
-model.compile(optimizer=tf.keras.optimizers.RMSprop(),
+model.compile(optimizer=tf.keras.optimizers.RMSprop(0.001),
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
               metrics=["accuracy"])
-cb = opt.LossCallback.LossCallback()
+cb = opt.LossCallback.LossCallback(early_stopping=True)
+"""
 model.fit(
     X,y,
-    epochs=1,
-    batch_size=128,
+    epochs=2,
+    batch_size=64,
     callbacks=[cb],
     validation_data=(X_test,y_test)
 )
 opt.utils.print_model(model,learning_metrics=cb.learning_metric)
-cb.plot_loss(new_figure=False)
+cb.plot_loss(new_figure=False, show=False)
+"""
+opt.set_default_misc(samples=6000,epochs=1,batch_size=64,learning_metric="LAST_LOSS",verbose=0)
+#model = opt.opt_loss_fun(model, X, y,categorical=True)
+model = opt.opt_all_layer_params(model, X, y, "strides")
+model = opt.opt_optimizer_parameter(model, X, y, "learning_rate")
+model = opt.opt_all_layer_params(model, X, y, "units")
+model = opt.opt_all_layer_params(model, X, y, "filters")
+model = opt.opt_all_layer_params(model, X, y, "pool_size")
+model = opt.opt_all_layer_params(model, X, y, "kernel_size")
+model = opt.opt_all_layer_params(model, X, y, "activation")
+model = opt.opt_optimizer_parameter(model, X, y, "decay")
+model = opt.opt_optimizer_parameter(model, X, y, "momentum")
+model = opt.opt_optimizer_parameter(model, X, y, "rho")
 
-configurations.configure(samples=6000,epochs=1,batch_size=128,learning_metric="NEG_ACCURACY",verbose=0)
-model = opt.opt_loss_fun(model, X, y,categorical=True,return_metric="RELATIVE_IMPROVEMENT_EPOCH")
-model = opt.opt_all_activations(model, X, y)
-model = opt.opt_all_units(model, X, y)
-model = opt.opt_learning_rate(model, X, y)
-model = opt.opt_decay(model, X, y)
+
 model.compile(optimizer=model.optimizer,loss=model.loss,metrics=["accuracy"]) #Must be compiled again if you want to add metrics
 cb = opt.LossCallback.LossCallback()
 hist = model.fit(
     X,y,
-    epochs=1,
-    batch_size=128,
+    epochs=2,
+    batch_size=64,
     callbacks=[cb],
     validation_data=(X_test,y_test)
 )
+print(opt.utils.elapsed_times)
 cb.plot_loss(show=True,new_figure=False)
+plt.figure()
+plt.plot(opt.utils.elapsed_times,range(len(opt.utils.elapsed_times)))
+plt.show()
 opt.utils.print_model(model,learning_metrics=cb.learning_metric)
 
 
